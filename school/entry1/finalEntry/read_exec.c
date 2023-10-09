802f471894e55afec5d75220bb8336a242e44868
Popinpip42 <luisxl10@gmail.com>
2023-10-09 23:39:40 +0200

    commit de prueba, trabajando en la entrega...

--- school/entry1/finalEntry/read_exec.c
+++ school/entry1/finalEntry/read_exec.c
@@ -12,56 +12,24 @@ int	main(void)
 {
 	int	bytes_read;
 	//char	*buff = NULL;
+	char	buff[BUFF_SIZE];
 
 	int	nums[2];
 	int	nums_size;
 	int	index;
 
 	nums_size = sizeof(nums) / sizeof(*nums);
-	index = 0;
-	while ((bytes_read = get_next_line(0, &buff)) != -1) //&& index < nums_size)
+	
+	while ((bytes_read = read(0, &buff, 10)) != -1) //&& index < nums_size)
 	{
-		printf("%s\n", buff);
-		printf("reading\n");
-
-		free(buff);
-		if (buff == NULL)
-			printf("IS NULL\n");
-		else
-			printf("faile to null\n");
-		fflush(stdin);
-	}
-}
-
-int	get_next_line(int fd, char **buffer)
-{
-	int	n_read;
-	int	ch;
-	int	i;
-
-	if ((*buffer = (char *)malloc(BUFF_SIZE * sizeof(char))) == NULL)
-	{
-		printf("MALLOC\n");
-		return (-1);
-	}
-	i = 0;
-	while ((n_read = read(fd, &ch, 1)) > 0)
-	{
-		printf("here\n");
-		if (ch == '\n')
+		index = 0;
+		printf("Read...: ");
+		while (buff[index] != '\0' && buff[index] != '\n')
 		{
-			(*buffer)[i] = '\0';		
-			//printf("%ld\n", strlen(*buffer));
-			return (strlen(*buffer));
+			printf("%c", buff[index]);
+			index++;
 		}
-		else
-		{
-			(*buffer)[i] = ch;
-			i++;
-		}
+		printf("\n");
 	}
-	free(*buffer);
-	printf("%d\n", n_read);
-	return (n_read);
 }
 
